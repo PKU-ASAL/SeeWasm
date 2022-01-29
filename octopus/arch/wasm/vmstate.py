60291f9c7872a299ea29347287d5d1ae43dcce37
am009 <634960348@qq.com>
2022-01-29 18:42:55 +0800

    Merge WASI related functions into WASIFunction. Add a command line argument for stdin input.

--- octopus/arch/wasm/vmstate.py
+++ octopus/arch/wasm/vmstate.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from octopus.engine.engine import VMstate
 from z3 import *
 
+STDIN_BYTES = b'123\n'
 
 class WasmVMstate(VMstate):
     def __init__(self):
@@ -26,7 +27,7 @@ class WasmVMstate(VMstate):
         # TODO adapt C code
         # TODO we insert a `123` here, maybe we should insert a symbol
         # stdin buffer used by _fd_read, but scanf(modeled seperately) does not read from it
-        self.stdin_buffer = [c for c in b'123\n'] # int array, use pop(0)
+        self.stdin_buffer = [c for c in STDIN_BYTES] # int array, use pop(0)
 
     def translate(self, ctx):
         state = WasmVMstate()
