7b3c3008b3a0089941d9b39e63310b69c03d498a
Ningyu He <pluton.he@gmail.com>
2022-03-10 11:16:47 +0800

    remove some redundant files

--- octopus/tests/wasm_run_tests.sh
+++
@@ -1,4 +0,0 @@
-echo '[*] WebAssembly Disassembler [*]'
-python3 -m unittest WASM/test_disassembler.py
-echo '[*] WebAssembly ControlFlowGraph & CallGraph analysis [*]'
-python3 -m unittest WASM/test_cfg.py
