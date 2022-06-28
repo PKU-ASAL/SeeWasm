4189c48e4ebfe7fb805ec6fc39c56596a4f9ae0f
Ningyu He <pluton.he@gmail.com>
2022-06-28 15:24:50 +0800

    eliminate unnecessary deepcopy

--- eunomia/arch/wasm/instructions/ParametricInstructions.py
+++ eunomia/arch/wasm/instructions/ParametricInstructions.py
@@ -23,12 +23,12 @@ class ParametricInstructions:
                 state.symbolic_stack.append(arg1 if arg0 == 0 else arg2)
                 return [state]
             else:
-                state1 = copy.deepcopy(state)
-                state1.constraints.append(simplify(arg0 != 0))
-                state1.symbolic_stack.append(arg1)
-                state2 = copy.deepcopy(state)
-                state2.constraints.append(simplify(arg0 == 0))
-                state2.symbolic_stack.append(arg2)
-                return [state1, state2]
+                new_state = copy.deepcopy(state)
+                new_state.constraints.append(simplify(arg0 != 0))
+                new_state.symbolic_stack.append(arg1)
+
+                state.constraints.append(simplify(arg0 == 0))
+                state.symbolic_stack.append(arg2)
+                return [state, new_state]
         else:
             raise UnsupportInstructionError
