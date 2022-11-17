cd6204efde126afa7e046bfcd2dd55fb2e235de0
Ningyu He <pluton.he@gmail.com>
2022-11-17 23:00:24 +0800

    remove graph related

--- eunomia_entry.py
+++ eunomia_entry.py
@@ -47,9 +47,8 @@ def main():
 
     features = parser.add_argument_group('Features')
     features.add_argument(
-        '-v', '--verbose', default='warning', const='warning', nargs='?',
-        choices=['warning', 'info', 'debug'],
-        help='set the logging level. debug is instruction level, info is function level')
+        '--entry', type=str, nargs=1, default=["__original_main"],
+        help='set entry point as the specilized function')
     features.add_argument(
         '--visualize', action='store_true',
         help='visualize the ICFG on basic blocks level')
@@ -65,6 +64,10 @@ def main():
     features.add_argument(
         '--dsl', action='store_true',
         help='Indicating the file is instrumented by dsl')
+    features.add_argument(
+        '-v', '--verbose', default='warning', const='warning', nargs='?',
+        choices=['warning', 'info', 'debug'],
+        help='set the logging level. debug is instruction level, info is function level')
 
     # vulnerability detector
     lasers = parser.add_argument_group('Lasers')
@@ -79,25 +82,9 @@ def main():
         help="fire the buffer overflow vulnerability detector")
 
     analyze = parser.add_argument_group('Analyze')
-    analyze = analyze.add_mutually_exclusive_group()
     analyze.add_argument(
         '-s', '--symbolic', action='store_true',
         help='perform the symbolic execution')
-    analyze.add_argument(
-        '-g', '--cfg', action='store_true',
-        help='generate the control flow graph (CFG) (instruction level)')
-    analyze.add_argument(
-        '-c', '--call', action='store_true',
-        help='generate the call flow graph (function level)')
-
-    graph = parser.add_argument_group('Graph options')
-    graph.add_argument('--simplify', action='store_true',
-                       help='generate a simplify CFG')
-    graph.add_argument('--functions', action='store_true',
-                       help='create subgraph for each function')
-    graph.add_argument(
-        '--onlyfunc', type=str, nargs=1, default=["__original_main"],
-        help='only generate the CFG for this list of function name')
 
     args = parser.parse_args()
 
@@ -107,7 +94,7 @@ def main():
     if args.symbolic:
         Configuration.set_verbose_flag(args.verbose)
         Configuration.set_file(args.file.name)
-        Configuration.set_entry(args.onlyfunc)
+        Configuration.set_entry(args.entry)
         Configuration.set_coverage(args.coverage)
         Configuration.set_visualize(args.visualize)
         Configuration.set_lasers(args.overflow, args.divzero, args.buffer)
@@ -144,26 +131,9 @@ def main():
 
             print(f"The visualization of ICFG is done.")
             return
+
         graph = Graph()
         graph.traverse()
-    # Visualize CFG or Call Graph
-    elif args.cfg or args.call:
-        from eunomia.analysis.graph import CFGGraph
-        from eunomia.arch.wasm.cfg import WasmCFG
-
-        octo_cfg = WasmCFG(octo_bytecode)
-
-        if args.call:
-            octo_cfg.visualize_call_flow()
-
-        if args.cfg:
-            octo_graph = CFGGraph(octo_cfg)
-            if args.functions or args.onlyfunc:
-                octo_graph.view_functions(only_func_name=args.onlyfunc,
-                                          simplify=args.simplify
-                                          )
-            else:
-                octo_graph.view(simplify=args.simplify)
     else:
         parser.print_help()
 
